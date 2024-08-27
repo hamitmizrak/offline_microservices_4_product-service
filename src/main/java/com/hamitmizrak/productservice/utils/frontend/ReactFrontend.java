6964b694ac1c6371c3926cbade5e334667507a41
Hamit Mızrak <hamitmizrak@gmail.com>
2024-08-27 21:58:02 +0300

    revize

---
+++ src/main/java/com/hamitmizrak/productservice/utils/frontend/ReactFrontend.java
@@ -0,0 +1,7 @@
+package com.hamitmizrak.productservice.utils.frontend;
+
+public class ReactFrontend {
+
+    // Const
+    public static final String PRODUCTION_REACT_FRONTEND_PORT_URL="http://localhost:3333";
+}
