9bc19ee3124b97187a4de3da74659011cb05b48f
Hamit Mızrak <hamitmizrak@gmail.com>
2024-08-27 23:15:29 +0300

    Dto,

---
+++ src/main/java/com/hamitmizrak/productservice/business/dto/ProductDto.java
@@ -0,0 +1,29 @@
+package com.hamitmizrak.productservice.business.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+import java.io.Serializable;
+import java.util.Date;
+
+// LOMBOK
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Log4j2
+
+public class ProductDto implements Serializable {
+
+    // Serileştirme
+    public static final Long serialVersionUID = 1L;
+
+    private Long id;
+    private String productName;
+    private double productPrice;
+    private int productQuantify;
+    private Date createDate;
+}
