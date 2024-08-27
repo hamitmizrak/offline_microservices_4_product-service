066389033601205ca19e7f1fbd4d1aeb32f85e2f
Hamit Mızrak <hamitmizrak@gmail.com>
2024-08-27 23:15:56 +0300

    IProductServices.java

---
+++ src/main/java/com/hamitmizrak/productservice/business/services/IProductServices.java
@@ -0,0 +1,29 @@
+package com.hamitmizrak.productservice.business.services;
+
+import java.util.List;
+
+// D: Dto
+// E: Entity
+public interface IProductServices <D,E>{
+
+    // Model Mapper
+    public D EntityToDto(E e);
+    public E DtoToEntity(D d);
+
+    ///////////////////////////////////////////
+    // C R U D
+    // CREATE
+    public D productServiceCreate(D d);
+
+    // LIST
+    public List<D> productServiceList();
+
+    // FIND BY ID
+    public D productServiceFindById(Long id);
+
+    // UPDATE
+    public D productServiceUpdate(Long id, D d);
+
+    // DELETE
+    public D productServiceDelete(Long id);
+} //end IProductServices
